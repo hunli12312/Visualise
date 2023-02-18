5a6c85221c137171eb7322129466a1fa4e5ed4cf
hunli12312 <121043448+hunli12312@users.noreply.github.com>
2023-02-18 15:20:22 +0100

    Add files via upload

---
+++ hackernews.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+from operator import itemgetter
+
+# Make an API call, and store the response.
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Explore the structure of the data.
+response_ids = r.json()
+submission_dicts = []
+for submission_id in response_ids[0:30]:
+    # Make a seperate API call for each submission.
+    url = ('https://hacker-news.firebaseio.com/v0/item/' +
+           str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.status_code)
+    response_dict = submission_r.json()
+
+    submission_dict = {
+        'title': response_dict['title'],
+        'link': 'https://hacker-news.firebaseio.com/v0/item' + str(submission_id),
+        'comments': response_dict.get('descendants', 0)
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+                          reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Link: {submission_dict['link']}")
+    print(f"Comments: {submission_dict['comments']}")
